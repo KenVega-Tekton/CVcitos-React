1ca05b02b7c95235c5f83ba959eab68076312aeb
Ken Vega <ken.vega@tektonlabs.com>
2018-02-15 14:49:49 -0500

    includin react-router

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,21 @@
 import React, { Component } from "react";
+import { BrowserRouter, Route } from "react-router-dom"; // to navigate the dom
+
+const Header = () => <h2>header</h2>;
+const Landing = () => <h2>Landing</h2>;
+const Dashboard = () => <h2>Dashboard</h2>;
 
 class App extends Component {
   render() {
     return (
       <div>
-        App works!
-        <p>Title</p>
+        <BrowserRouter>
+          <div>
+            <Header />
+            <Route exact path="/" component={Landing} />
+            <Route path="/dash" component={Dashboard} />
+          </div>
+        </BrowserRouter>
       </div>
     );
   }
